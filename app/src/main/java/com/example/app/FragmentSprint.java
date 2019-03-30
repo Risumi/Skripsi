bc157efde402e2a373dad47a41c16ff8d4f9fa8d
Risumi <rizkysuhaimi@gmail.com>
2019-03-30 23:47:04 +0700

    Added initial sprint fragment code

--- app/src/main/java/com/example/app/FragmentSprint.java
+++ app/src/main/java/com/example/app/FragmentSprint.java
@@ -3,9 +3,17 @@ package com.example.app;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 
 /**
@@ -13,7 +21,7 @@ import android.view.ViewGroup;
  * Use the {@link FragmentSprint#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragmentSprint extends Fragment {
+public class FragmentSprint extends Fragment implements BacklogAdapter.BacklogViewHolder.ClickListener, SprintAdapter.SprintViewHolder.ClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -23,6 +31,13 @@ public class FragmentSprint extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private ArrayList<Backlog>listBacklog, BacklogToDo,BacklogOnProgress,BacklogCompleted;
+    private RecyclerView mRecyclerView;
+    private RecyclerView mRecyclerView2;
+    private RecyclerView mRecyclerView3;
+    private SprintAdapter mAdapter;
+    private SprintAdapter mAdapter2;
+    private SprintAdapter mAdapter3;
 
     public FragmentSprint() {
         // Required empty public constructor
@@ -58,8 +73,63 @@ public class FragmentSprint extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sprint, container, false);
+        View view = inflater.inflate(R.layout.fragment_sprint, container, false);
+
+        listBacklog = new ArrayList<>();
+        listBacklog.add(new Backlog("Recycler View","Completed", Calendar.getInstance().getTime(),Calendar.getInstance().getTime(),"","Membuat recycler view untuk menampilkan list backlog serta menghapus backlog "));
+        listBacklog.add(new Backlog("Recycler View","Completed", Calendar.getInstance().getTime(),Calendar.getInstance().getTime(),"","Membuat recycler view untuk menampilkan list backlog serta menghapus backlog "));
+        listBacklog.add(new Backlog("Recycler View","Completed", Calendar.getInstance().getTime(),Calendar.getInstance().getTime(),"","Membuat recycler view untuk menampilkan list backlog serta menghapus backlog "));
+        listBacklog.add(new Backlog("Burndown Chart","On Progress", Calendar.getInstance().getTime(),Calendar.getInstance().getTime(),"","Membuat chart untuk merepresentasikan backlog ke dalam bentuk chart sesuai dengan kaidah scrum"));
+        listBacklog.add(new Backlog("Sprint","To Do", Calendar.getInstance().getTime(),Calendar.getInstance().getTime(),"","Membuat automatisasi proses sprint"));
+        listBacklog.add(new Backlog("Sprint","To Do", Calendar.getInstance().getTime(),Calendar.getInstance().getTime(),"","Membuat automatisasi proses sprint"));
+        BacklogToDo = new ArrayList<>();
+        BacklogOnProgress = new ArrayList<>();
+        BacklogCompleted= new ArrayList<>();
+        setBacklog(listBacklog);
+
+        mRecyclerView = view.findViewById(R.id.rvToDo);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        int resId = R.anim.layout_animation_fall_down;
+        LayoutAnimationController animation = AnimationUtils.loadLayoutAnimation(view.getContext(), resId);
+        mAdapter = new SprintAdapter(view.getContext(), BacklogToDo,this);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutAnimation(animation);
+
+        mRecyclerView2 = view.findViewById(R.id.rvProgress);
+        mRecyclerView2.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        mAdapter2 = new SprintAdapter(view.getContext(), BacklogOnProgress,this);
+        mRecyclerView2.setAdapter(mAdapter2);
+        mRecyclerView2.setLayoutAnimation(animation);
+
+        mRecyclerView3 = view.findViewById(R.id.rvCompleted);
+        mRecyclerView3.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        mAdapter3 = new SprintAdapter(view.getContext(), BacklogCompleted,this);
+        mRecyclerView3.setAdapter(mAdapter3);
+        mRecyclerView3.setLayoutAnimation(animation);
+
+
+        return view;
     }
 
+    @Override
+    public void onItemClicked(int position) {
+
+    }
+
+    @Override
+    public boolean onItemLongClicked(int position) {
+        return false;
+    }
+
+    public void setBacklog(ArrayList<Backlog> listBacklog){
+        for (int i=0;i<listBacklog.size();i++){
+            if (listBacklog.get(i).getStatus().equalsIgnoreCase("To Do")){
+                BacklogToDo.add(listBacklog.get(i));
+            }else if (listBacklog.get(i).getStatus().equalsIgnoreCase("On Progress")){
+                BacklogOnProgress.add(listBacklog.get(i));
+            }else if (listBacklog.get(i).getStatus().equalsIgnoreCase("Completed")){
+                BacklogCompleted.add(listBacklog.get(i));
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/example/app/SprintAdapter.java
@@ -0,0 +1,137 @@
+package com.example.app;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SprintAdapter extends SelectableAdapter<SprintAdapter.SprintViewHolder>{
+
+    LayoutInflater mInflater;
+    ArrayList<Backlog> backlogArrayList;
+    Context _context;
+    Backlog current;
+    private SprintViewHolder.ClickListener clickListener;
+
+    public SprintAdapter(Context _context, ArrayList<Backlog> backlogArrayList, SprintViewHolder.ClickListener clickListener) {
+        this.mInflater = LayoutInflater.from(_context);
+        this.backlogArrayList = backlogArrayList;
+        this._context = _context;
+        this.clickListener = clickListener;
+    }
+
+    @NonNull
+    @Override
+    public SprintViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_sprint, parent, false);
+        return new SprintViewHolder(v,this,clickListener);
+    }
+
+    @Override
+    public void onBindViewHolder(SprintViewHolder holder, int position) {
+        current = backlogArrayList.get(position);
+        SimpleDateFormat formatDate = new SimpleDateFormat("dd MMMM yyyy");
+        String endda= formatDate.format(current.getEndda().getTime());
+        holder.BacklogName.setText(current.getName());
+        holder.BacklogDate.setText(endda);
+//        holder.selectedOverlay.setVisibility(isSelected(position)?View.VISIBLE : View.INVISIBLE);
+    }
+
+    @Override
+    public int getItemCount() {
+        return backlogArrayList.size();
+    }
+
+    public void removeItem(int position) {
+        backlogArrayList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void removeItems(List<Integer> positions) {
+        // Reverse-sort the list
+        Collections.sort(positions, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer lhs, Integer rhs) {
+                return rhs - lhs;
+            }
+        });
+
+        // Split the list in ranges
+        while (!positions.isEmpty()) {
+            if (positions.size() == 1) {
+                removeItem(positions.get(0));
+                positions.remove(0);
+            } else {
+                int count = 1;
+                while (positions.size() > count && positions.get(count).equals(positions.get(count - 1) - 1)) {
+                    ++count;
+                }
+
+                if (count == 1) {
+                    removeItem(positions.get(0));
+                } else {
+                    removeRange(positions.get(count - 1), count);
+                }
+
+                for (int i = 0; i < count; ++i) {
+                    positions.remove(0);
+                }
+            }
+        }
+    }
+
+    private void removeRange(int positionStart, int itemCount) {
+        for (int i = 0; i < itemCount; ++i) {
+            backlogArrayList.remove(positionStart);
+        }
+        notifyItemRangeRemoved(positionStart, itemCount);
+    }
+
+    static class SprintViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener,View.OnLongClickListener{
+        TextView BacklogName;
+        TextView BacklogDate;
+        SprintAdapter mAdapter;
+        View selectedOverlay;
+        private ClickListener listener;
+
+        public SprintViewHolder(View itemView, SprintAdapter adapter,ClickListener listener) {
+            super(itemView);
+            BacklogName = itemView.findViewById(R.id.txtName);
+            BacklogDate = itemView.findViewById(R.id.txtDate);
+            this.listener = listener;
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
+            this.mAdapter = adapter;
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (listener != null) {
+                listener.onItemClicked(getPosition());
+            }
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            if (listener != null) {
+                return listener.onItemLongClicked(getPosition());
+            }
+            return false;
+        }
+
+        public interface ClickListener {
+            public void onItemClicked(int position);
+            public boolean onItemLongClicked(int position);
+        }
+    }
+}
